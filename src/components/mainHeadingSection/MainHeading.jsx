30055ad8061cdc06e0665189c40dc1e69076c9b1
dreamlabo <toddlabo@gmail.com>
2022-12-06 21:27:35 -0700

    Create-Main-Dashboard-Navigation: Added styles to section.

--- src/components/mainHeadingSection/MainHeading.jsx
+++ src/components/mainHeadingSection/MainHeading.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './mainHeading.css'
+import HeaderIcon from '../../images/dashboard-symbol.png'
 
 const MainHeading = () => {
   return (
-    <div></div>
+    <div className='evms__main-heading_wrapper'>
+        <div className="evms__main-heading_container">
+            <div className="evms__main-heading">
+                <img src={HeaderIcon} alt="dashboard icon" />
+                <h1>Main Dashboard</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
