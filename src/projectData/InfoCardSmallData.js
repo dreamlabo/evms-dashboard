9dcdccd1fb114ab6b73dc47115474c1308f0a5a2
dreamlabo <toddlabo@gmail.com>
2022-12-27 22:18:43 -0700

    Refactor-Info-Cards: Refactored Small Info Cards so the data is pulled from an array of objects rather than 'hard code'.

---
+++ src/projectData/InfoCardSmallData.js
@@ -0,0 +1,29 @@
+const dataInfoCardSmall = [
+    {
+        'cardTitle': 'Original Estimate Cost',
+        'titleValue': 15940.04,
+        'headingOne': 'Time Spent Cost',
+        'headingOneValue': 16919.93,
+        'headingTwo': 'Remaining Estimate Cost',
+        'headingTwoValue': 1712.43,
+        'accentColor': '248, 96%, 61%' 
+    },
+    {
+        'cardTitle': 'Original Estimate Price',
+        'titleValue': 21957.76,
+        'headingOne': 'Time Spent Cost',
+        'headingOneValue': 18335.25,
+        'headingTwo': 'Remaining Estimate Cost',
+        'headingTwoValue': 1854.77,
+        'accentColor': '266, 100%, 64%'
+    },
+    {
+        'cardTitle': 'Budget',
+        'titleValue': 15940.04,
+        'headingOne': 'Actual Margin',
+        'headingOneValue': 6847.32,
+        'accentColor': '175, 98%, 24%'
+    }
+]
+
+export default dataInfoCardSmall;
