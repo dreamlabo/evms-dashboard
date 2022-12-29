64abef1ff91cdcb977380ef1645374cd90c12a1a
dreamlabo <toddlabo@gmail.com>
2022-12-28 22:52:44 -0700

    Refactor-Project-...: Refactored so that when clicking a nav link in the secondary navigation, the list of projects change correctly.

--- src/projectData/infoCardData.js
+++ src/projectData/infoCardData.js
@@ -1,4 +1,4 @@
-const infoCardData = [
+export const UnderperformingInfoCardData = [
     {
         'projectInitials': 'AS',
         'projectname': 'Accounting System',
@@ -13,12 +13,89 @@ const infoCardData = [
         'projectInitials': 'ST',
         'projectname': 'Sunrise Test',
         'status': 'Executing',
-        'spi': 1.58,
-        'cpi': 1.01,
+        'spi': 0.54,
+        'cpi': 0.41,
         'originalEstimate': 193.22,
         'timeSpent': 186.00,
         'remainingEstimate': 102.00,
     }
 ]
 
-export default infoCardData;
+export const AtRiskInfoCardData = [
+    {
+        'projectInitials': 'LS',
+        'projectname': 'Lightning Strikes',
+        'status': 'Executing',
+        'spi': 0.45,
+        'cpi': 1.01,
+        'originalEstimate': 14451.07,
+        'timeSpent': 1402.87,
+        'remainingEstimate': 1681.07,
+    },
+
+    {
+        'projectInitials': 'AD',
+        'projectname': 'Algorithmic Design',
+        'status': 'Executing',
+        'spi': 0.78,
+        'cpi': 1.04,
+        'originalEstimate': 13451.07,
+        'timeSpent': 1302.87,
+        'remainingEstimate': 1481.07,
+    },
+    {
+        'projectInitials': 'UC',
+        'projectname': 'Ultra Course',
+        'status': 'Executing',
+        'spi': 0.65,
+        'cpi': 1.04,
+        'originalEstimate': 1451.07,
+        'timeSpent': 102.87,
+        'remainingEstimate': 161.07,
+    },
+]
+
+export const OverperformingInfoCardData = [
+    {
+        'projectInitials': 'WS',
+        'projectname': 'Western States',
+        'status': 'Executing',
+        'spi': 1.76,
+        'cpi': 1.01,
+        'originalEstimate': 14351.07,
+        'timeSpent': 1402.87,
+        'remainingEstimate': 181.07,
+    },
+    {
+        'projectInitials': 'MT',
+        'projectname': 'Miwok Trail',
+        'status': 'Executing',
+        'spi': 2.38,
+        'cpi': 1.94,
+        'originalEstimate': 17451.37,
+        'timeSpent': 10002.87,
+        'remainingEstimate': 1341.75,
+    },
+    {
+        'projectInitials': 'AR',
+        'projectname': 'American River',
+        'status': 'Executing',
+        'spi': 1.55,
+        'cpi': 1.94,
+        'originalEstimate': 18765.07,
+        'timeSpent': 1004.87,
+        'remainingEstimate': 161.07,
+    },
+    {
+        'projectInitials': 'GT',
+        'projectname': 'Grand Traverse',
+        'status': 'Executing',
+        'spi': 2.38,
+        'cpi': 1.43,
+        'originalEstimate': 10145.61,
+        'timeSpent': 1674.46,
+        'remainingEstimate': 151.89,
+    }
+]
+
+// export default UnderperformingInfoCardData;
